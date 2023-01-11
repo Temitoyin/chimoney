15bc5ed1bd5f8100c9492cf42e89f631528212ff
Temitoyin <tjayorinde@gmail.com>
2023-01-11 12:31:14 +0100

    initail project setup

---
+++ src/pages/checkout/checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './checkout.module.scss';
+
+ const Checkout = () => {
+  return (
+    <div>checkout</div>
+  )
+}
+
+
+
+export default Checkout;
---
+++ src/pages/productListing/productListing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './productListing.module.scss';
+const ProductListing = () => {
+  return (
+    <div>productListing</div>
+  )
+}
+
+export default ProductListing;
